2dfa899334887efd12206b4853de4b2a2768a9ad
unknown <khomich10@mail.ru>
2020-06-18 15:47:33 +0300

    add styles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,16 +10,26 @@ export default class App extends React.Component {
  
 
   state = {
-    photo: "",
-    date: new Date()
+    photo: 'https://apod.nasa.gov/apod/image/2005/PorpoiseGalaxy_HubbleFraile_1300.jpg',
+    date: new Date() , 
+    dateFirst: null
   }
+
+
+ 
+  
 
   componentDidMount() {
     this.getData()
+
+  
+    console.log(this.state)
+
   }
 
   async getData  () {
     console.log(this.state.date)
+
 
     const fuultear = this.state.date.getFullYear()
     const day = this.state.date.getDate()
@@ -27,7 +37,6 @@ export default class App extends React.Component {
   
     const response = await fetch(`https://api.nasa.gov/planetary/apod?date=${fuultear}-${month}-${day}&hd=True&api_key=TmzKsJwGltpO9CFAxZbP4JFrG8QipD4IXChaMjmZ`);
     
-      console.log(response)
     if(!response.ok){
         throw new Error(`we have a problem with fetch`);
     }
@@ -40,41 +49,62 @@ export default class App extends React.Component {
 
 
 
-
-getInfo () {
-  this.getData()
-  .then((body) => {
-    console.log(body)
-    this.setState({
-      photo: body.url,
-    })
-  })
-}
-
   onChange = (date) => {
+   
+    console.log(this.state.date)
+
+    
+  
     this.setState({ 
       date,
-     })
+    })
+    
+    console.log(this.state.date)
 
-     this.getInfo()
-     console.log(this.state.photo)
-
-     console.log(this.state)
+    this.getData(date)
+     .then((body) => {
+       console.log(body)
+       console.log(this.state)
+      //  sessionStorage.setItem('firstdate', this.state.date)
+       this.setState({
+         photo: body.url,
+         dateFirst: sessionStorage.getItem('firstdate')
+       })
+      })
+      
   
-
+      sessionStorage.setItem('firstdate', this.state.date)
   }
 
   
 
   render(){
 
-    const {photo} = this.state;
+    let {photo} = this.state;
 
+    console.log(photo)
     
+    if (photo.indexOf("apod.nasa.gov") === -1) {
+      return (
+      <div className="App">
+          {/* <img src='https://apod.nasa.gov/apod/image/1403/m78_sharp_960.jpg' alt="photo_of_day"/> */}
+          <div className="AppImage">
+            <p>Не удалось открыть фото.</p> 
+            <p>Возможно указанный источник не существует.</p>
+            <p>Пожалуйста, выберите другую дату.</p>
+            </div>
+          <Calendar 
+              onChange={this.onChange}
+              value={this.state.date}
+              
+          />
+      </div>
+      )
+    }
 
     return (
       <div className="App">
-          <img src={photo} alt="kj" className="imageApp"/>
+          <img src={photo} alt="photo_of_day"/>
           <Calendar 
               onChange={this.onChange}
               value={this.state.date}
