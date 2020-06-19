3641a91835d41389351552cd5f8ed3c2d2a7b28c
unknown <khomich10@mail.ru>
2020-06-19 15:26:29 +0300

    Updates

---
+++ precache-manifest.7cb1612dd480ac50cd5cdb4c92208602.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ec09a9f87770590ed5a64a108f140c6",
+    "url": "/apod_react/index.html"
+  },
+  {
+    "revision": "9221d5e07192069c7088",
+    "url": "/apod_react/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "1a6aa2fa4f1d97fcf333",
+    "url": "/apod_react/static/css/main.830085e3.chunk.css"
+  },
+  {
+    "revision": "9221d5e07192069c7088",
+    "url": "/apod_react/static/js/2.e4e9a54a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/apod_react/static/js/2.e4e9a54a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a6aa2fa4f1d97fcf333",
+    "url": "/apod_react/static/js/main.a54de9bb.chunk.js"
+  },
+  {
+    "revision": "68a30df9300e1362eb29",
+    "url": "/apod_react/static/js/runtime-main.5e21d2a3.js"
+  }
+]);
